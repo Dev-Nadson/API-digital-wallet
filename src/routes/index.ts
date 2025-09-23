4711cb897fe07b1bad125420bfb4169bbb0607e1
Dev Nadson <nadson.pros@gmail.com>
2025-09-23 19:27:42 -0300

    Feat: /user prefix adicionado

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,7 @@ import type { FastifyInstance } from 'fastify';
 import { user_routes } from './user.routes.js';
 
 async function app_routes(app: FastifyInstance) {
-    app.register(user_routes)
+    app.register(user_routes, { prefix: '/user' })
 }
 
 export { app_routes }
