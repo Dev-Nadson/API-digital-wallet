1d0b87acbf7cf748218c58f9af31562b87d8424e
Dev Nadson <nadson.pros@gmail.com>
2025-09-23 19:26:15 -0300

    Feat: rotas de usuário criadas

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,8 +1,14 @@
 import type { FastifyInstance } from "fastify"
 import { create_user_controller } from "../controllers/users/create-user.controller.js"
+import { list_user_controller } from "../controllers/users/list-user.controller.js"
+import { update_user_controller } from "../controllers/users/update-user.controller.js"
+import { delete_user_controller } from "../controllers/users/delete-user.controller.js"
 
 async function user_routes(app: FastifyInstance) {
-    app.get("/", create_user_controller)
+    app.post("/create", create_user_controller)
+    app.get("/list", list_user_controller)
+    app.put("/update", update_user_controller)
+    app.delete("/delete", delete_user_controller)
 }
 
 export { user_routes }
