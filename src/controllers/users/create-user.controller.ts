b9dca56dcbdebe76bedcca5b6579ea662e03d2eb
Dev Nadson <nadson.pros@gmail.com>
2025-09-23 19:10:38 -0300

    Fix: Mensagem corrigida

--- src/controllers/users/create-user.controller.ts
+++ src/controllers/users/create-user.controller.ts
@@ -2,7 +2,7 @@ import type { FastifyRequest, FastifyReply } from "fastify";
 
 async function create_user_controller(req: FastifyRequest, reply: FastifyReply) {
     const sum = (a: number, b: number) => a + b;
-    return reply.status(201).send(`Controller funconando ${sum(1, 2)}`)
+    return reply.status(201).send(`Controller funcionando ${sum(1, 2)}`)
 }
 
 export { create_user_controller }
