6a68663696a9945e485f3142c5b91222a5a863c3
Dev-Nadson <nadson.pros@gmail.com>
2025-09-27 20:19:38 -0300

    Feat: função de lista adicionada

--- src/controllers/users/list-user.controller.ts
+++ src/controllers/users/list-user.controller.ts
@@ -1,7 +1,10 @@
 import type { FastifyRequest, FastifyReply } from "fastify";
+import { list_user_repositorie } from "../../repositories/users/list-user.repositorie.js";
 
+//mantive o req para futura paginação
 async function list_user_controller(req: FastifyRequest, reply: FastifyReply) {
-    return reply.status(200).send(() => console.log("Listed"))
+    const users = await list_user_repositorie()
+    return reply.status(200).send(users)
 }
 
 export { list_user_controller }
