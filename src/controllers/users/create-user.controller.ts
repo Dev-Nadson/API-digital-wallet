c240e2e56484de22cbc3e96ac2b69f6d004e2818
Dev-Nadson <nadson.pros@gmail.com>
2025-09-27 20:01:43 -0300

    Feat: Tipagem adicionada e insert ao banco de dados feito

--- src/controllers/users/create-user.controller.ts
+++ src/controllers/users/create-user.controller.ts
@@ -1,8 +1,14 @@
 import type { FastifyRequest, FastifyReply } from "fastify";
+import type { Iuser } from "../../types/user.types.js";
+import { create_user_repositorie } from "../../repositories/users/create-user.repositorie.js";
 
-async function create_user_controller(req: FastifyRequest, reply: FastifyReply) {
-    const sum = (a: number, b: number) => a + b;
-    return reply.status(201).send(`Controller funcionando ${sum(1, 2)}`)
+async function create_user_controller(
+    req: FastifyRequest<{ Body: Iuser }>, //generics types
+    reply: FastifyReply
+) {
+    const { name, email } = req.body
+    await create_user_repositorie({ "name": name, "email": email })
+    return reply.status(201).send(() => console.log("Usuário criado!"))
 }
 
 export { create_user_controller }
