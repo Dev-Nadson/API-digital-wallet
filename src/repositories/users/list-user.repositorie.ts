068ba01b4c78f4cf33449225be5fc8cef829b5c5
Dev-Nadson <nadson.pros@gmail.com>
2025-09-27 20:24:40 -0300

    Feat: ordenado por data mais recente

--- src/repositories/users/list-user.repositorie.ts
+++ src/repositories/users/list-user.repositorie.ts
@@ -1,7 +1,7 @@
 import { Knex } from "../../database/config.js";
 
 async function list_user_repositorie() {
-    return await Knex("users").select()
+    return await Knex("users").select().orderBy("created_at", "desc")
 }
 
 export { list_user_repositorie }
