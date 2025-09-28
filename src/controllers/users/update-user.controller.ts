51d9896a5ad005e127834bbe375da5a1b3d90307
Dev-Nadson <nadson.pros@gmail.com>
2025-09-27 21:04:34 -0300

    Feat: Tipagem adicionada, desestruturação e retorno correto

--- src/controllers/users/update-user.controller.ts
+++ src/controllers/users/update-user.controller.ts
@@ -1,7 +1,16 @@
 import type { FastifyRequest, FastifyReply } from "fastify";
+import type { Iuser, IUpdateUserParams } from "../../types/user.types.js";
+import { update_user_repositorie } from "../../repositories/users/update-user.repositorie.js";
 
-async function update_user_controller(req: FastifyRequest, reply: FastifyReply) {
-    return reply.status(200).send(() => console.log("Updated"))
+async function update_user_controller(
+    req: FastifyRequest<{ Body: Iuser, Params: IUpdateUserParams }>,
+    reply: FastifyReply
+) {
+    const { name, email } = req.body
+    const { id } = req.params
+    await update_user_repositorie({ "name": name, "email": email }, id)
+
+    return reply.status(200).send("Usuário atualizado")
 }
 
 export { update_user_controller }
