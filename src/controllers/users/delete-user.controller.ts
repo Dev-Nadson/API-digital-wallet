42c78cf41218730b370c16142365ea3f5e3e0c3d
Dev-Nadson <nadson.pros@gmail.com>
2025-09-27 21:27:47 -0300

    Feat: Tipagem adicionada, desestruturação feita e repositorie adicionado

--- src/controllers/users/delete-user.controller.ts
+++ src/controllers/users/delete-user.controller.ts
@@ -1,7 +1,11 @@
 import type { FastifyRequest, FastifyReply } from "fastify";
+import type { IUpdateUserParams } from "../../types/user.types.js";
+import { delete_user_repositorie } from "../../repositories/users/delete-user.repositorie.js";
 
-async function delete_user_controller(req: FastifyRequest, reply: FastifyReply) {
-    return reply.status(200).send(() => console.log("Deleted"))
+async function delete_user_controller(req: FastifyRequest<{ Params: IUpdateUserParams }>, reply: FastifyReply) {
+    const { id } = req.params
+    await delete_user_repositorie(id)
+    return reply.status(204).send(() => console.log("Deleted"))
 }
 
 export { delete_user_controller }
