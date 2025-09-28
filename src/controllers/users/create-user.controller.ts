556a6feffb1380bc60148922d77b49771f945b95
Dev-Nadson <nadson.pros@gmail.com>
2025-09-28 20:51:11 -0300

    Feat: Inicio do body schema

--- src/controllers/users/create-user.controller.ts
+++ src/controllers/users/create-user.controller.ts
@@ -1,14 +1,25 @@
 import type { FastifyRequest, FastifyReply } from "fastify";
 import type { Iuser } from "../../types/user.types.js";
 import { create_user_repositorie } from "../../repositories/users/create-user.repositorie.js";
+import { hash_text } from "../../utils/encryption.js";
+
+import z, { email } from 'zod'
+
+const user_schema = z.object({
+    name: z.string(),
+    email: z.email(),
+    password: z.string(),
+    balance: z.number().optional()
+})
 
 async function create_user_controller(
     req: FastifyRequest<{ Body: Iuser }>, //generics types
     reply: FastifyReply
 ) {
     const { name, email, password } = req.body
-    await create_user_repositorie({ "name": name, "email": email, "password": password })
-    return reply.status(201).send("Usuário criado!")
+    const hash_password = await hash_text(password)
+    const data = await create_user_repositorie({ "name": name, "email": email, "password": hash_password })
+    return reply.status(201).send(data)
 }
 
 export { create_user_controller }
