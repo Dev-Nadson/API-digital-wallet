c653d05939058bda493bbb84ea5d9f6d671a7e2a
Dev-Nadson <nadson.pros@gmail.com>
2025-10-02 18:34:08 -0300

    Feat: Rotas para transação adicionadas

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import type { FastifyInstance } from 'fastify';
 
+import { transantions_routes } from './transations.routes.js';
 import { user_routes } from './user.routes.js';
 
 async function app_routes(app: FastifyInstance) {
     app.register(user_routes, { prefix: '/users' })
+    app.register(transantions_routes, { prefix: '/transations' })
 }
 
 export { app_routes }
--- src/routes/transations.routes.ts
+++ src/routes/transations.routes.ts
@@ -1,6 +1,10 @@
 import type { FastifyInstance } from "fastify"
+import { create_transation_controller } from "../controllers/transations/create-transation.controller.js"
+import { list_transation_controller } from "../controllers/transations/list-transation.controller.js"
 
 async function transantions_routes(app: FastifyInstance) {
+    app.post("/create", create_transation_controller)
+    app.get("/list", list_transation_controller)
 }
 
 export { transantions_routes }
