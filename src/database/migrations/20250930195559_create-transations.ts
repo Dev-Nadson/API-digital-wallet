a12f45894fa2b5f8ab2e4d202dcf710bfa10aec7
Dev-Nadson <nadson.pros@gmail.com>
2025-10-02 18:08:12 -0300

    Feat: Tabela das transações adicionada

--- src/database/migrations/20250930195559_create-transations.ts
+++ src/database/migrations/20250930195559_create-transations.ts
@@ -1,10 +1,17 @@
 import type { Knex } from "knex";
 
-
 export async function up(knex: Knex): Promise<void> {
+    await knex.schema.createTable("users-transations", (table) => {
+        table.increments("id").primary()
+        table.integer("sender_id").references("id").inTable("users").notNullable()
+        table.integer("receiver_id").references("id").inTable("users").notNullable()
+        table.decimal("amount", 10, 2).notNullable()
+        table.timestamp("created_at").defaultTo(knex.fn.now()).notNullable()
+    })
 }
 
 
 export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTable("users-transations")
 }
 
