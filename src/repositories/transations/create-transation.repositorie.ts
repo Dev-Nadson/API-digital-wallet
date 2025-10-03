51b11050d61869cb04cd42929b9bb6ef2b04410e
Dev-Nadson <nadson.pros@gmail.com>
2025-10-03 00:11:39 -0300

    Feat: lógica de negócio inicial

--- src/repositories/transations/create-transation.repositorie.ts
+++ src/repositories/transations/create-transation.repositorie.ts
@@ -1,8 +1,14 @@
 import { Knex } from "../../database/config.js";
-import type { Itransation, ItransationParams } from "../../types/user.types.js";
+import type { Itransation } from "../../types/user.types.js";
 
-async function create_transation_repositorie(transation: Itransation & ItransationParams) {
+async function create_transation_repositorie(transation: Itransation) {
     const { sender_id, receiver_id, amount } = transation
+    const receiver_balance = await Knex("users").select("balance").where({ "id": sender_id }).first()
+    const new_balance = receiver_balance.balance + amount
+    await Knex("users").update({ "balance": new_balance }).where({ "id": receiver_id })
+    console.log(receiver_balance)
+    console.log(amount)
+
     await Knex("users-transations").insert({ sender_id, receiver_id, amount })
 }
 
