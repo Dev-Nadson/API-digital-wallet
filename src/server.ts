fb417568866d3271db0f71eda2589c33886c5620
Dev-Nadson <nadson.pros@gmail.com>
2025-10-03 13:45:18 -0300

    Fix: porta 3333

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
 import fastify from "fastify"
 import { app_routes } from "./routes/index.js"
 
-const PORT = 4000
+const PORT = 3333
 const app = fastify()
 
 app.register(app_routes)
 
 try {
-    app.listen({ "port": PORT }, () => {
+    await app.listen({ "port": PORT }, () => {
         console.log(`Server running on port ${PORT}`)
     })
 } catch (err) {
