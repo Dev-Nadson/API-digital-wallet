28518695885c1a87572a9c5cb03330a7a7417a26
Dev-Nadson <nadson.pros@gmail.com>
2025-10-03 13:48:00 -0300

    Feat: Rota de listar transações por usuário

--- src/routes/transations.routes.ts
+++ src/routes/transations.routes.ts
@@ -1,10 +1,12 @@
 import type { FastifyInstance } from "fastify"
 import { create_transation_controller } from "../controllers/transations/create-transation.controller.js"
 import { list_transation_controller } from "../controllers/transations/list-transation.controller.js"
+import { list_user_transation_controller } from "../controllers/transations/list-user-transation.controller.js"
 
-async function transantions_routes(app: FastifyInstance) {
-    app.post("/create/:id?amount=&receiver=", create_transation_controller)
+async function transations_routes(app: FastifyInstance) {
+    app.post("/:sender_id/create", create_transation_controller)
     app.get("/list", list_transation_controller)
+    app.get("/:sender_id/list", list_user_transation_controller)
 }
 
-export { transantions_routes }
+export { transations_routes }
