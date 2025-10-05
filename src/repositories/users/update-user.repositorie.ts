c631bdb258ba1712d7c3284f0da9a64d4b827df1
Dev-Nadson <nadson.pros@gmail.com>
2025-10-05 01:29:48 -0300

    Feat: Validação de erro de usuário não existente adicionada

--- src/repositories/users/update-user.repositorie.ts
+++ src/repositories/users/update-user.repositorie.ts
@@ -1,9 +1,19 @@
 import { Knex } from "../../database/config.js";
-import type { Iuser } from "../../types/user.types.js";
+import type { Iuser, IUpdateUserParams } from "../../types/user.types.js";
 
-async function update_user_repositorie(user: Iuser, id: number) {
+async function update_user_repositorie(user: Iuser, req_id: IUpdateUserParams) {
     const { name, email, password } = user
+    const { id } = req_id
+
+    const exist_user = await Knex("users").select().where({ "id": id })
+    console.log(exist_user)
+
+    if (!exist_user) {
+        return "USER_DONT_EXISTS"
+    }
+
     await Knex("users").update({ name, email, password }).where({ "id": id })
+    return await Knex("users").select().where({ "id": id })
 }
 
 export { update_user_repositorie }
