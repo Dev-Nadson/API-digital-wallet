616ab842cb8c26f8c0c1111b547a505eafe31099
Dev-Nadson <nadson.pros@gmail.com>
2025-10-04 23:55:18 -0300

    Fix: Error movido para handler e para o server.ts

--- src/controllers/users/create-user.controller.ts
+++ src/controllers/users/create-user.controller.ts
@@ -3,55 +3,39 @@ import type { Iuser } from "../../types/user.types.js";
 import { create_user_repositorie } from "../../repositories/users/create-user.repositorie.js";
 import { hash_text } from "../../utils/encryption.js";
 import { user_schema } from "../../schemas/user.schema.js";
-import { ZodError } from "zod";
-import { z } from "zod";
 
 async function create_user_controller(
     req: FastifyRequest<{ Body: Iuser }>, //generics types
     reply: FastifyReply
 ) {
+
+    const { name, email, password } = user_schema.parse(req.body)
+
+    let hash_password: string
     try {
-        const { name, email, password } = user_schema.parse(req.body)
-
-        let hash_password: string
-        try {
-            hash_password = await hash_text(password)
-        } catch (error) {
-            console.error("Hash error:", error)
-            return reply.status(500).send({
-                error: "Internal server error",
-                message: "Hash password error"
-            })
-        }
-
-        const user = await create_user_repositorie({ name, email, "password": hash_password })
-
-        if (user === "EMAIL_ALREADY_EXISTS") {
-            return reply.status(409).send({
-                message: "Conflict Error",
-                data: user
-            })
-        }
-
-        return reply.status(201).send({
-            message: "User created with success",
-            data: user
-        })
-
+        hash_password = await hash_text(password)
     } catch (error) {
-
-        if (error instanceof ZodError) {
-            return reply.status(400).send({
-                error: "Valitation error",
-                issues: z.treeifyError(error)
-            })
-        }
-
+        console.error("Hash error:", error)
         return reply.status(500).send({
             error: "Internal server error",
-            message: "An unexpected error has ocurred"
+            message: "Hash password error"
         })
     }
+
+    const user = await create_user_repositorie({ name, email, "password": hash_password })
+
+    if (user === "EMAIL_ALREADY_EXISTS") {
+        return reply.status(409).send({
+            message: "Conflict Error",
+            data: user
+        })
+    }
+
+    return reply.status(201).send({
+        message: "User created with success",
+        data: user
+    })
+
 }
 
 export { create_user_controller }
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
 import fastify from "fastify"
 import { app_routes } from "./routes/index.js"
+import { error_handler_middleware } from "./middlewares/error-handler-middleware.js"
 
 const PORT = 3333
 const app = fastify()
 
 app.register(app_routes)
 
-try {
-    await app.listen({ "port": PORT }, () => {
-        console.log(`Server running on port ${PORT}`)
-    })
-} catch (err) {
-    console.log("An error occurred", err)
-}
+app.setErrorHandler(error_handler_middleware)
+
+await app.listen({ "port": PORT }, () => {
+    console.log(`Server running on port ${PORT}`)
+})
+
