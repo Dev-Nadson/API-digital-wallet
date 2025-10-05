aa9b49f3e95a18dd69397d23a0a6d11abe9d0121
Dev-Nadson <nadson.pros@gmail.com>
2025-10-05 01:28:57 -0300

    Feat: Validação de erro de usuário não existente adicionada

--- src/controllers/users/update-user.controller.ts
+++ src/controllers/users/update-user.controller.ts
@@ -2,17 +2,38 @@ import type { FastifyRequest, FastifyReply } from "fastify";
 import type { Iuser, IUpdateUserParams } from "../../types/user.types.js";
 import { update_user_repositorie } from "../../repositories/users/update-user.repositorie.js";
 import { hash_text } from "../../utils/encryption.js";
+import { user_schema, update_user_schema } from "../../schemas/user.schema.js";
 
 async function update_user_controller(
     req: FastifyRequest<{ Body: Iuser, Params: IUpdateUserParams }>,
     reply: FastifyReply
 ) {
-    const { name, email, password } = req.body
-    const { id } = req.params
-    const hash_password = await hash_text(password)
-    await update_user_repositorie({ "name": name, "email": email, "password": hash_password }, id)
 
-    return reply.status(200).send("Usuário atualizado")
+    const { name, email, password } = user_schema.parse(req.body)
+    const { id } = update_user_schema.parse(req.params)
+    const number_id = Number(id)
+
+    let hash_password: string
+    try {
+        hash_password = await hash_text(password)
+    } catch (error) {
+        console.error("Hash error:", error)
+        return reply.status(500).send({
+            error: "Internal server error",
+            message: "Hash password error"
+        })
+    }
+
+    const user = await update_user_repositorie({ "name": name, "email": email, "password": hash_password }, { "id": number_id })
+    console.log(user)
+    if (user === "USER_DONT_EXISTS") {
+        return reply.status(404).send({
+            message: "non-existent user",
+            data: user
+        })
+    }
+
+    return reply.status(200).send(user)
 }
 
 export { update_user_controller }
