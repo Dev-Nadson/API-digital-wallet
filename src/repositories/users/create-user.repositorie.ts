667c85d4395f97a5a7d5474154ee1e8d39bf66c1
Dev-Nadson <nadson.pros@gmail.com>
2025-10-04 22:12:56 -0300

    Feat: adicionada verificação de email disponível

--- src/repositories/users/create-user.repositorie.ts
+++ src/repositories/users/create-user.repositorie.ts
@@ -1,9 +1,23 @@
 import { Knex } from "../../database/config.js";
 import type { Iuser } from "../../types/user.types.js";
+import { verify_unique_email } from "../../utils/verify-unique-email.js";
 
 async function create_user_repositorie(user: Iuser) {
-    const { name, email, password } = user
-    await Knex("users").insert({ name, email, password })
+    try {
+        const { name, email, password } = user
+
+        const exists_email = await verify_unique_email(email);
+
+        if (exists_email) {
+            return "EMAIL_ALREADY_EXISTS"
+        }
+
+        await Knex("users").insert({ name, email, password })
+        return await Knex("users").select("").where({ email })
+    } catch (error) {
+
+    }
+
 }
 
 export { create_user_repositorie }
