bef432eaa6092194b0064c0aaaddd1f3747c4f5d
Dev-Nadson <nadson.pros@gmail.com>
2025-10-05 21:45:28 -0300

    Feat: Tratamento de erros adicionado

--- src/controllers/transactions/create-transaction.controller.ts
+++ src/controllers/transactions/create-transaction.controller.ts
@@ -1,13 +1,47 @@
 import type { FastifyRequest, FastifyReply } from "fastify";
 import { create_transaction_repositorie } from "../../repositories/transactions/create-transaction.repositorie.js";
-import type { Itransaction, ItransactionParams } from "../../types/user.types.js";
+import { transaction_params_schema, transaction_schema } from "../../schemas/transaction.schema.js";
+import type { Itransaction, ItransactionParams } from "../../types/transaction.types.js";
 
 async function create_transaction_controller(req: FastifyRequest<{ Body: Itransaction, Params: ItransactionParams }>, reply: FastifyReply) {
-    const { receiver_id, amount } = req.body
-    const { sender_id } = req.params
+    const { receiver_id, amount } = transaction_schema.parse(req.body)
+    const { sender_id } = transaction_params_schema.parse(req.params)
     const sender_id_number = Number(sender_id)
 
-    await create_transaction_repositorie({ sender_id_number, receiver_id, amount })
-    return reply.status(201).send("Transação concluída")
+    if (sender_id_number === receiver_id) {
+        return reply.status(400).send({
+            message: "Bad Request",
+            data: "CANNOT_TRANSFER_TO_YOURSELF"
+        })
+    }
+
+    const transaction = await create_transaction_repositorie({ sender_id_number, receiver_id, amount })
+
+    if (transaction === "SENDER_DONT_EXISTS") {
+        return reply.status(404).send({
+            message: "non-existent sender",
+            data: transaction
+        })
+    }
+
+    if (transaction === "RECEIVER_DONT_EXISTS") {
+        return reply.status(404).send({
+            message: "non-existent receiver",
+            data: transaction
+        })
+    }
+
+    if (transaction === "INSUFFICIENT_BALANCE") {
+        return reply.status(400).send({
+            message: "insufficient balance",
+            data: transaction
+        })
+    }
+
+    return reply.status(201).send({
+        message: "Transaction completed successfully",
+        data: transaction
+    })
 }
-export { create_transaction_controller } 
+
+export { create_transaction_controller }
--- src/repositories/transactions/create-transaction.repositorie.ts
+++ src/repositories/transactions/create-transaction.repositorie.ts
@@ -1,8 +1,26 @@
 import { Knex } from "../../database/config.js";
-import type { Itransaction } from "../../types/user.types.js";
+import type { Itransaction } from "../../types/transaction.types.js";
+import { verify_user_exists } from "../../utils/verify-user-exists.js";
+
+// Se sender.balance < amount, retornar erro "INSUFFICIENT_BALANCE"
 
 async function create_transaction_repositorie(transaction: Itransaction) {
     const { sender_id_number, receiver_id, amount } = transaction
+
+    const exist_sender = await verify_user_exists(sender_id_number)
+    if (!exist_sender) {
+        return "SENDER_DONT_EXISTS"
+    }
+
+    const exist_receiver = await verify_user_exists(receiver_id)
+    if (!exist_receiver) {
+        return "RECEIVER_DONT_EXISTS"
+    }
+
+    const balance = await Knex("users").select("balance").where({ "id": sender_id_number }).first()
+    if (balance.balance < amount) {
+        return "INSUFFICIENT_BALANCE"
+    }
 
     await Knex.transaction(async (trx) => {
         await trx("users").where({ "id": receiver_id }).increment("balance", amount)
@@ -10,6 +28,8 @@ async function create_transaction_repositorie(transaction: Itransaction) {
 
         await trx("users-transations").insert({ "sender_id": sender_id_number, receiver_id, amount })
     })
+
+    return Knex("users-transations").select().first().orderBy("id", "desc")
 }
 
 export { create_transaction_repositorie }
